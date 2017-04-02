5cfbc0242864e70d699b6766c58e3fb99c002633
Alexia Young <alexia.kwok@gmail.com>
2017-04-02 14:29:57 -0500

    seeded quotes from google forms

--- server/api/profiles.js
+++ server/api/profiles.js
@@ -7,23 +7,23 @@ const Alexia = {
 
 const Christine = {
   cat: '/images/Christine_cat.jpg',
-  happiest: '',
-  funniest: '',
-  quote: ''
+  happiest: 'Super pod during review week.  Seeing everyone\'s pets on Slack.  Epic Burger outing.',
+  funniest: 'Ray shouting "I\'m Merlin! Soren\'s evil!!" \n Sorting us all into Hogwarts Houses. \n Cookie crew/first time Nick brought us cookies.',
+  quote: '“I never had a group of friends before, I promise that I’ll make y’all proud.”'
 };
 
 const Connie = {
   cat: '/images/Connie_cat.png',
-  happiest: '',
-  funniest: '',
-  quote: ''
+  happiest: 'When Alexia brought those brownie cookies - happiest moment of my life',
+  funniest: 'Nick\'s story of Dave bullying Baxter',
+  quote: 'you all are so awesome, I\'ll be so sad when ya\'ll leave...! \n stay!!!!!!'
 };
 
 const David = {
   cat: '/images/David_cat.gif',
-  happiest: '',
-  funniest: '',
-  quote: ''
+  happiest: 'Review week was so tight',
+  funniest: 'Nick Lee\'s entire hot seat',
+  quote: 'Y\'all ain\'t bad'
 };
 
 const Don = {
@@ -35,9 +35,9 @@ const Don = {
 
 const Mack = {
   cat: '/images/Mack_cat.gif',
-  happiest: '',
-  funniest: '',
-  quote: ''
+  happiest: 'Joining Sytherin, Top House ',
+  funniest: '¯\_(ツ)_/¯  (will think of one later)',
+  quote: 'Everyone has been awesome to work with and be around! I\'ve never worked with a nicer group of individuals in my 25 years!! \n Sailing this Summer! yeehaw!'
 };
 
 const Meg = {
@@ -63,16 +63,16 @@ const Ray = {
 
 const Rob = {
   cat: '/images/Rob_cat.jpg',
-  happiest: '',
-  funniest: '',
-  quote: ''
+  happiest: 'Cute animals all the time, game nights, COOKIES, making Game of Life, Alexia\'s snax',
+  funniest: 'Testing our capstone project, watching Ray and Soren and Hal do karaoke',
+  quote: 'Y\'all are awesome! I\'m excited to see where everyone ends up!'
 };
 
 const Sara = {
   cat: '/images/Sara_cat.png',
-  happiest: '',
-  funniest: '',
-  quote: ''
+  happiest: 'Bonding and having a lot of fun in the super pod during Review Week',
+  funniest: 'There are a lot, but Nick Lee\'s hot seat was pretty hilarious',
+  quote: 'Thank you for being a friend.  *cue Golden Girls*  \n Have a neat summer.  KIT.  <3, Sara'
 };
 
 const Soren = {
